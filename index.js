a13172ce967f53ef45e1b66e96d38343db2e10b8
友人Ｓ <YuuJinS@Project.CSJ>
2021-12-28 17:30:26 +0900

    Set up Router

---
+++ index.js
@@ -0,0 +1,87 @@
+/* eslint-disable no-tabs */
+// Port Setting
+const port = 80;
+
+// Express
+const express = require(
+	"express",
+);
+const app = express();
+app.use(
+	express.static(
+		"public",
+	),
+);
+
+// ejs Engine
+const engine = require(
+	"ejs-locals",
+);
+app.engine(
+	"ejs",
+	engine,
+);
+app.set(
+	"views",
+	"./views",
+);
+app.set(
+	"view engine",
+	"ejs",
+);
+
+// Color Log
+const logger = require(
+	"node-color-log",
+);
+logger.setLevel(
+	"info",
+);
+logger.setDate(
+	() => new Date()
+		.toLocaleString(),
+);
+
+// Router
+app.get(
+	"/", function(
+		req,
+		res,
+	) {
+		logger.info(
+			"Someone comes to our site's home page",
+		);
+		res.render(
+			"index",
+			{
+				title: "Home",
+			},
+		);
+	},
+);
+
+app.get(
+	"/exit",
+	function(
+		req,
+		res,
+	) {
+		logger.warn(
+			"Process Stop!",
+		);
+		process.exit(
+			0,
+		);
+	},
+);
+
+// Process Start
+logger.info(
+	"Starting Process",
+);
+app.listen(
+	port,
+);
+logger.info(
+	"Process Start!",
+);
